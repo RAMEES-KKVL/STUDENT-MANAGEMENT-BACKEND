342a9b734db5f8978261026c4ae74fb7e794af5b
RAMEES-KKVL <rameesp41750@gmail.com>
2024-06-22 13:10:09 +0530

    user login validation implemented with jwt

--- controller/authController.ts
+++ controller/authController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
+import { type Request, type Response } from "express";
 import { signupData } from "../models/signup";
 import * as bcrypt from 'bcryptjs'
 import sendOTPEmail from "../utilities/nodemailer.service"
+import jwt from "jsonwebtoken"
 
 export default {
     signupPost : async (req: Request, res: Response) =>{
@@ -52,6 +53,7 @@ export default {
                         const newUser = await newSchema.save()
                         
                         if ( newUser ){
+                            // Sending OTP through nodemailer
                             const otp = 1234
                             sendOTPEmail(email, otp)
                             return res.status(200).json({ success : true, isDataSaved : true })
@@ -62,12 +64,68 @@ export default {
                     
                 } catch (error) {
                     // catching the error
-                    console.log(error)
                     return res.status(400).json({ message: 'Server error' })
                 }
             }
         }
-    }
+    },
+
+    loginPost: async (req: Request, res: Response)=>{
+        const { email, password } = req.body
+
+        if ( !email || !password ) {
+            return res.status(400).json({ success : false, missingData : true, message : "Provide reequired datas" })
+        } else {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8}$/
+            const isEmailValid = emailRegex.test(email)
+            const isPasswordValid = passwordRegex.test(password)
+
+            // Backend validation
+            if ( !isEmailValid ) {
+                return res.status(422).json({ success : false, invalidEmail : true, message : "Please enter a valid email address." })
+            } else if ( !isPasswordValid ) {
+                return res.status(422).json({ success : false, invalidPassword : true, message : "Please enter a valid password" })
+            } else {
+
+                try {
+                    const existingUser = await signupData.findOne({ email })
+
+                    if ( !existingUser ) {
+                        return res.status(404).json({ success : false, noUser : true, message : "User does not exist" })
+                    } else {
+                        const passMatch = await bcrypt.compare( password, existingUser.password )
+                        if ( !passMatch ) {
+                            return res.status(401).json({ success : false, invalidPassword : true, message : "Password does not match" })
+                        } else {
+                            if ( existingUser.isVerified ) {
+                                if ( existingUser.isBlocked ) {
+                                    return res.status(403).json({ success : false, isBlocked : true, message : "Login failed. Try again later" })
+                                } else {
+                                    // Creating user token
+                                    const payload = {
+                                        user : existingUser
+                                    }
+                                    const secret = process.env.jwt_secret as string
+                                    const token = jwt.sign(payload, secret, { expiresIn: "48h" })
+                                    return res.status(200).json({ success : true, user : true, token })
+                                }   
+                            } else {
+                                // Sending OTP through nodemailer
+                                const otp = 1234
+                                sendOTPEmail(email, otp)
+                                return res.status(200).json({ success : true, notVerified : true })
+                            }
+                        }
+                    }
+                } catch ( error ) {
+                    // catching error 
+                    return res.status(400).json({ success : false, message : "Server error" })
+                }
+            }
+        }
+    },
+
 }
 
 
--- router/authRouter.ts
+++ router/authRouter.ts
@@ -3,5 +3,6 @@ import authController from "../controller/authController"
 const Router = express.Router()
 
 Router.post("/signup", authController.signupPost)
+Router.post("/login", authController.loginPost)
     
 export default Router
